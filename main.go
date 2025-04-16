d3ae1b5ed74869f3592cfb5b0a6a44f24c903cea
Rahul Marban <rahulmarban@gmail.com>
2025-04-16 23:48:59 +0530

    Update main.go

--- main.go
+++ main.go
@@ -359,8 +359,8 @@ func updateLatexTemplate(githubData *models.GithubResponse, linkedinData *models
 		}
 
 		entry := []string{
-			fmt.Sprintf("\\textbf{%s} %s \\hfill %s\\\\", cleanData(degreeType), edu.FieldOfStudy, endDate),
 			fmt.Sprintf("\\href{%s}{%s} \\hfill \\textit{CGPA: %s}", edu.Url, cleanData(schoolName), cleanData(edu.Grade)),
+			fmt.Sprintf("\\textbf{%s} %s \\hfill %s\\\\", cleanData(degreeType), edu.FieldOfStudy, endDate),
 		}
 
 		educationEntries = append(educationEntries, strings.Join(entry, "\n"))
